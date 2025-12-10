2eda52fa84f77d447baec3cfd6a788f6b94c2f83
voidnamespace <voidnamespace@outlook.com>
2025-12-10 10:26:13 +0400

    insight1

--- src/BankCardService.Application/Services/BankCardService.cs
+++ src/BankCardService.Application/Services/BankCardService.cs
@@ -2,111 +2,15 @@
 using BankCardService.Application.Interfaces;
 using BankCardService.Domain.Entities;
 using BankCardService.Domain.ValueObjects;
-using Microsoft.Extensions.Logging;
+
 
 namespace BankCardService.Application.Service;
 
-public class BankCardService : IBankCardService
+public class BankCardService
 {
-    private readonly ILogger<BankCardService> _logger;
-    private readonly BankCardDbContext _context;
-
-    public BankCardService (ILogger<BankCardService> logger, BankCardDbContext context)
-    {
-        _logger = logger;
-        _context = context;
-    }
     
-    public async Task<BankCardDTO> CreateAsync(CreateBankCardDTO bankCardDTO)
-    {
-        if (bankCardDTO == null)
-            throw new ArgumentNullException("Bank card can not be empty");
-        var cardNumber = bankCardDTO.CardNumber;
-        var cardHolder = bankCardDTO.CardHolder;
-        var newCard = await _context.BankCards.CreateAsync(cardNumber, cardHolder);
-        await _context.SaveChangesAsync();
-        var returnCard = new BankCardDTO
-        {
-            Id = newCard.Id,
-            CardNumber = newCard.CardNumber,
-            CardHolder = newCard.CardHolder,
-            ExpirationDate = newCard.ExpirationDate,
-            Balance = newCard.Balance
-        };
-        return returnCard;
-    }
-    
-    public async Task DepositAsync(Guid cardId, decimal amount)
-    {
-        var card = await _context.BankCards.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        card.Deposit(amount);
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task WithdrawalAsync(Guid cardId, decimal amount)
-    {
-        var card = await _context.BankCards.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        card.Withdrawal(amount);
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task ActivateAsync(Guid cardId)
-    {
-        var card = await _context.BankCards.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        card.Activate();
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task DeactivateAsync(Guid cardId)
-    {
-        var card = await _context.BankCards.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        card.Deactivate();
-        await _context.SaveChangesAsync();
-    }
-    public async Task ChangeCardHolderAsync(Guid cardId, string newHolder)
-    {
-        var card = await _context.BankCards.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        card.ChangeCardHolder(newHolder);
-        await _context.SaveChangesAsync();
-    }
-    public async Task ChangeCardNumberAsync(Guid cardId, string newNumber)
-    {
-        var card = await _context.BankCards.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        var cardNumberVo = new CardNumberVO(newNumber);
-        card.ChangeCardNumber(cardNumberVo);
-        await _context.SaveChangesAsync();
-    }
-    public async Task<BankCardDTO> GetByIdAsync (Guid cardId)
-    {
-        var card = await _context.BankCard.FindAsync(cardId);
-        if (card == null)
-            throw new KeyNotFoundException("Bank card not found");
-        var cardDTO = new BankCardDTO
-        {
-            Id = cardId,
-            CardNumber = card.Number,
-            CardHolder = card.CardHolder,
-            ExpirationDate = card.ExpirationDate,
-            Balance = card.Balance,
-            IsActive = card.IsActive,
-        };
-        return cardDTO;
-    }
-    public async Task<IEnumerable<BankCardDTO>> GetAllAsync()
-    {
-        var cards = await _context.BankCards.ToListAsync();
-        return cards;
-    }
 }
+
+    
+  
+
